980d90d8b85934a989d222d06a70203c7e88f7ee
Denys <den9.volkov@gmail.com>
2025-02-10 12:47:02 +0200

    fix: rename constructor for matrix3x3

--- matrix3x3.go
+++ matrix3x3.go
@@ -6,7 +6,7 @@ type Matrix3x3 struct {
 	c2 Vector3
 }
 
-func NewFloat3x3(c0, c1, c2 Vector3) Matrix3x3 {
+func NewMatrix3x3(c0, c1, c2 Vector3) Matrix3x3 {
 	return Matrix3x3{
 		c0: c0,
 		c1: c1,
